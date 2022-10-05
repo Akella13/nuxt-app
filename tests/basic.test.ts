ac1a7a94b71ceefd85aa00c8c8759d56341f6397
Arman_Kozhakhmetov <akella_piter@mail.ru>
2022-10-05 13:24:58 +0300

    feat: vitest init

---
+++ tests/basic.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest'
+import { setup, $fetch, isDev } from '@nuxt/test-utils-edge'
+import { fileURLToPath } from 'node:url'
+
+describe('example', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL('..', import.meta.url)),
+    server: true
+  })
+
+  it('Renders Hello Nuxt', async () => {
+    expect(await $fetch('/')).toMatch('Welcome to the homepage')
+  })
+
+  if (isDev()) {
+    it('[dev] ensure vite client script is added', async () => {
+      expect(await $fetch('/')).toMatch('/_nuxt/@vite/client"')
+    })
+  }
+})
