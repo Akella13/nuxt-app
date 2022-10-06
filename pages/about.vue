22348782dbd0b6a1fe03d990033a17c198efb27f
Arman_Kozhakhmetov <akella_piter@mail.ru>
2022-10-06 14:20:06 +0300

    feat: ssr init

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,12 @@
 <template>
   <section>
     <p>This page will be displayed at the /about route.</p>
+    <NuxtExampleLayout example="server/routes">
+      <p>Fetched from <pre>/api/mountain</pre>: {{ mountain }}</p>
+    </NuxtExampleLayout>
   </section>
 </template>
+
+<script setup>
+  const { data: mountain } = await useFetch('/api/mountain')
+</script>
