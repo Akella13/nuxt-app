98a7a758123f7aac6b20b6f3a3085f6a60d6b23e
Arman_Kozhakhmetov <akella_piter@mail.ru>
2022-10-19 16:48:50 +0300

    feat: lastRoll() computed

--- pages/dnd/index.vue
+++ pages/dnd/index.vue
@@ -10,7 +10,10 @@
       </li>
     </ul>
     <div v-if="rollHistory.length > 0">
-      <h3>Your last roll: {{ rollHistory[rollHistory.length - 1].result }}</h3>
+      <h3>Your last roll: {{ lastRoll.result }}</h3>
+      <h4 v-if="lastRoll.die === 20 && (lastRoll.result === 1 || lastRoll.result === 20)">
+        Critical {{ lastRoll.result === 1 ? 'fail' : 'success' }}!
+      </h4>
     </div>
   </section>
 </template>
@@ -23,19 +26,30 @@
   })
 
   /** Pull of all possible dice */
-  const diceArr = useState('diceArr', (): dieSet => new Set([4, 6, 8, 10, 12, 20]))
+  const diceArr = useState<dieSet>('diceArr', () => new Set([4, 6, 8, 10, 12, 20]))
   /** Array of dice roll results */
   const rollHistory = useState<roll[]>('rollHistory', () => [])
+  /** Last roll object */
+  const lastRoll = computed(() => {
+    const obj = rollHistory.value[rollHistory.value.length - 1]
+    // if die is d20 and the result is either 1 or 20
+    if (obj?.die === 20 && (obj?.result === 1 || obj?.result === 20)) {
+      return {
+        ...obj,
+        critical: obj?.result === 1 ? 'fail' : 'success',
+      } 
+    }
+    return obj
+  })
 
   /** Random result of a die roll */
   const rollDie = (sides: die) => Math.round(Math.random()*(sides - 1)) + 1
 
   /** Handle specific die roll button */
   const DieButtonHandler = (die: die) => {
-    const result = rollDie(die)
     rollHistory.value.push({
       die,
-      result,
+      result: rollDie(die),
     })
   }
 </script>
