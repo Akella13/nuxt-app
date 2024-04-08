0012099ed3843962812fec1b388eb39d00d32558
Arman_Kozhakhmetov <akella_piter@mail.ru>
2024-04-08 12:59:26 +0500

    feat: SaveStats() to localStorage

--- components/Stats.vue
+++ components/Stats.vue
@@ -4,22 +4,45 @@
     <span v-show="stats.length === 0">
       Fetching stats ...
     </span>
-    <ul>
-      <li v-for="{ name, value } in stats">
-        <label>
-          <input type="radio"
-            name="stat"
-            :value="value"
-            @change="mod = calcMod(value)"
-          >
-          <b>{{ value }}</b>
-          {{ name }}
-        </label>
-      </li>
-    </ul>
+
+    <form @submit.prevent="SaveStats">
+      <ul>
+        <li v-for="({ name, value }, index) in stats">
+          <label>
+            <!-- HACK: v-model cannot be used on v-for scope vars -->
+            <input v-if="statsEditable"
+              v-model="stats[index].value"
+              type="number"
+              min="0"
+              max="20"
+            >
+            <template v-else>
+              <input type="radio"
+                name="stat"
+                :value="value"
+                @change="mod = calcMod(value)"
+              >
+              <b>{{ value }}</b>
+            </template>
+            {{ name }}
+          </label>
+        </li>
+      </ul>
+      <button v-if="statsEditable"
+        type="submit"
+      >
+        Save
+      </button>
+      <button v-else
+        @click="statsEditable = true"
+      >
+        Edit stats
+      </button>
+    </form>
+
     <h3>Your modifier: {{ mod }}</h3>
     <button v-show="mod !== 0"
-      @click="mod = 0"
+      @click="ResetMod"
     >
       Reset modifier
     </button>
@@ -34,4 +57,19 @@
 
   /** Modifier selected by user */
   const mod = useState('mod', () => 0)
+  
+  /** Set mod equal to 0 */
+  const ResetMod = () => {
+    mod.value = 0
+  }
+
+  /** Allows of stats editing */
+  const statsEditable = ref(false)
+
+  /** Save current value of stats to localStorage */
+  const SaveStats = () => {
+    localStorage.setItem('stats', JSON.stringify(stats.value))
+    ResetMod()
+    statsEditable.value = false
+  }
 </script>
