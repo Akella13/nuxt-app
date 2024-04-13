4cd2ac54c88d7b0224c6a54def32de88f522e62e
Arman_Kozhakhmetov <akella_piter@mail.ru>
2024-04-13 12:39:53 +0500

    feat: Roll & Roll20 classes

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -6,10 +6,10 @@ import {
 
 describe('calcMod', () => {
   test('Modifier of 16 is +3', () => {
-    expect(calcMod(16)).toBe(3)
+    expect(calcMod(16)).toEqual(3)
   })
   test('Modifier of 0 is -5', () => {
-    expect(calcMod(0)).toBe(-5)
+    expect(calcMod(0)).toEqual(-5)
   })
 })
 describe('selectRoll', () => {
@@ -24,5 +24,13 @@ describe('selectRoll', () => {
   })
   test('Straight roll (2, 3) => undefined', () => {
     expect(selectRoll(2,3)).toBe(undefined)
+  })
+})
+describe('rollDie', () => {
+  test('rolling d20 is <= 20', () => {
+    expect(rollDie(undefined)).toBeLessThanOrEqual(20)
+  })
+  test('rolling d4 is >= 1', () => {
+    expect(rollDie(4)).toBeGreaterThanOrEqual(1)
   })
 })
--- utils/index.ts
+++ utils/index.ts
@@ -9,27 +9,22 @@ import type {
 /** Modifier of a characteristic stat */
 export const calcMod  = (stat = 10) => Math.floor((stat - 10) / 2)
 
-/** Select one roll from multiple within one dice type */
-export const selectRoll = (
-  prev: number,
-  next: number,
-  adv: adv = advBook.straight
-): number | void => {
-  switch (adv) {
-    case advBook.adv: return Math.max(prev, next)
-    case advBook.dis: return Math.min(prev, next)
-    default: return
-  }
-}
-
 /** Random result of a die roll */
-const rollDie = (sides: die = 20) => Math.round(Math.random() * (sides - 1)) + 1
+export const rollDie = (sides: die = 20) => Math.round(Math.random() * (sides - 1)) + 1
+
+class Roll {
+  constructor(sides: die) {
+    this.natural = rollDie(sides)
+  }
+
+  natural
+}
 
 /** Result of rolling multiple die */
 const rollGroup = (dieArr: die[]) => {
   return dieArr.reduce((acc: rollMultiNat, val: die) => {
     /** Natural result of a single roll */
-    const natural = rollDie(val)
+    const { natural } = new Roll(val)
     // add result to group totalNat
     acc.totalNat += natural
     // add dice to group
@@ -52,14 +47,35 @@ const critRoll = (natural: number): crit | void => {
   }
 }
 
+class Roll20 extends Roll {
+  constructor(sides: die) {
+    super(sides)
+    this.critical = critRoll(this.natural)
+  }
+
+  critical
+}
+
+/** Select one roll from multiple within one dice type */
+export const selectRoll = (
+  prev: number,
+  next: number,
+  adv: adv = advBook.straight
+): number | void => {
+  switch (adv) {
+    case advBook.adv: return Math.max(prev, next)
+    case advBook.dis: return Math.min(prev, next)
+    default: return
+  }
+}
+
 /** Result of rolling multiple die */
 const rollGroup20 = (
   dieArr: die[],
   adv: adv = advBook.straight
 ) => {
   return dieArr.reduce((acc: rollMultiNat, val: die) => {
-    const natural = rollDie(val)
-    const critical = critRoll(natural)
+    const { natural, critical } = new Roll20(val)
     const selected = selectRoll(acc.totalNat, natural, adv)
     if (selected) {
       acc.totalNat = selected
